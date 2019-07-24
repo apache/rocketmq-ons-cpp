96edebaad904afa0422d8808789cbe13df7004af
ShannonDing <libya_003@163.com>
2019-07-24 20:52:05 +0800

    Add lisence header

--- src/test/java/org/apache/rocketmq/graalvm/CInterfaceTest.java
+++ src/test/java/org/apache/rocketmq/graalvm/CInterfaceTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.rocketmq.graalvm;
 
 import org.graalvm.nativeimage.c.type.CTypeConversion;
@@ -6,13 +23,13 @@ import org.mockito.Mockito;
 
 /**
  * <p>
- *     <strong>Warning:</strong> CTypeConversion can only used with native-image. Hence, we cannot create JUnit test
- *     cases in the Java side.
+ * <strong>Warning:</strong> CTypeConversion can only used with native-image. Hence, we cannot create JUnit test
+ * cases in the Java side.
  * </p>
  */
 public class CInterfaceTest {
 
-//    @Test
+    //    @Test
     public void testCreateProducer() {
         CInterface.CFactoryProperty property = Mockito.mock(CInterface.CFactoryProperty.class);
         Mockito.when(property.getAccessKey()).thenReturn(CTypeConversion.toCString("Ak").get());
@@ -24,7 +41,7 @@ public class CInterfaceTest {
         Assert.assertEquals(1, CInterface.instances.size());
     }
 
-//    @Test
+    //    @Test
     public void testDestroyInstance() {
         CInterface.CFactoryProperty property = Mockito.mock(CInterface.CFactoryProperty.class);
         Mockito.when(property.getAccessKey()).thenReturn(CTypeConversion.toCString("Ak").get());
